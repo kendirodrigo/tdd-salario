d32e613b898cd60d2a63a4f0416827bdb546bd32
Felipe Neves <ctl_fneves@uolinc.com>
2021-02-26 16:11:57 -0300

    Grupo 5 novos testes

---
+++ src/main/java/com/example/tddsalario/DBA.java
@@ -0,0 +1,4 @@
+package com.example.tddsalario;
+
+public class DBA {
+}
--- src/test/java/com/example/tddsalario/CalculadoraDeSalarioTest.java
+++ src/test/java/com/example/tddsalario/CalculadoraDeSalarioTest.java
@@ -3,27 +3,45 @@ package com.example.tddsalario;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class CalculadoraDeSalarioTest {
+class CalculadoraDeSalarioTest {
 
     @Test
-    public void deveCalcularSalarioParaDesenvolvedorComSalarioAbaixoDoLimite() throws Exception {
-        String nome = "";
-        String sobrenome = "";
-        Funcionario desenvolvedor = new Funcionario(nome, sobrenome, Cargo.DESENVOLVEDOR, 1500.0);
-        CalculadoraSalario calculadoraSalario = new CalculadoraSalario();
-        Double salarioLiquido = calculadoraSalario.calcularSalarioLiquido(desenvolvedor);
+    void deveCalcularSalarioParaDesenvolvedorComSalarioAbaixoDoLimite() throws Exception {
+        String nome = "Funcionario";
+        String sobrenome = "TesteAbaixo";
 
-        Assertions.assertEquals(1350, salarioLiquido);
+        Funcionario desenvolvedor = new Desenvolvedor(nome, sobrenome, Cargo.DESENVOLVEDOR, 1500.0);
+
+        Assertions.assertEquals(1350, desenvolvedor.getSalarioLiquido());
     }
 
-//    @Test
-//    public void deveCalcularSalarioParaDesenvolvedorComSalarioAcimaDoLimite() throws Exception {
-//        String nome = "";
-//        String sobrenome = "";
-//        Funcionario desenvolvedor = new Funcionario(nome, sobrenome, Cargo.DESENVOLVEDOR, new BigDecimal(1500.0));
-//        CalculadoraSalario calculadoraSalario = new CalculadoraSalario();
-//        BigDecimal salarioLiquido = calculadoraSalario.calcularSalarioLiquido(Cargo.DESENVOLVEDOR, 1500.0);
-//
-//        Assertions.assertEquals(1350, salarioLiquido);
-//    }
+    @Test
+    void deveCalcularSalarioParaDesenvolvedorComSalarioAcimaDoLimite() throws Exception {
+        String nome = "Funcionario";
+        String sobrenome = "TesteAcima";
+
+        Funcionario desenvolvedor = new Desenvolvedor(nome, sobrenome, Cargo.DESENVOLVEDOR, 3001.0);
+
+        Assertions.assertEquals(2400.8, desenvolvedor.getSalarioLiquido());
+    }
+
+    @Test
+    void deveCalcularSalarioParaDBAComSalarioAbaixoDoLimite() throws Exception {
+        String nome = "DBA";
+        String sobrenome = "TesteAbaixo";
+
+        Funcionario dba = new DBA(nome, sobrenome, Cargo.DBA, 2499.0);
+
+        Assertions.assertEquals(1350, dba.getSalarioLiquido());
+    }
+
+    @Test
+    void deveCalcularSalarioParaDBAComSalarioAcimaDoLimite() throws Exception {
+        String nome = "DBA";
+        String sobrenome = "TesteAcima";
+
+        Funcionario dba = new DBA(nome, sobrenome, Cargo.DBA, 3001.0);
+
+        Assertions.assertEquals(2400.8, dba.getSalarioLiquido());
+    }
 }
