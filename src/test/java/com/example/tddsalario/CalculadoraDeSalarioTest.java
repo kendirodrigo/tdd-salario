4f7aff22b0aea47a0cc066158304ce563fddabde
kendirodrigo <rkpereira@uolinc.com>
2021-02-26 14:35:49 -0300

    Adicionando classe de calculadora de salario test

---
+++ src/test/java/com/example/tddsalario/CalculadoraDeSalarioTest.java
@@ -0,0 +1,15 @@
+package com.example.tddsalario;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraDeSalarioTest {
+
+
+    @Test
+    public void deveCalcularSalarioParaDesenvolvedorComSalarioAbaixoDoLimite(){
+
+        Funcionario desenvolvedor = new Funcionario(Cargo.DESENVOLVEDOR, 1500.0);
+
+
+    }
+}
